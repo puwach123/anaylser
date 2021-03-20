494d16e3f17026184037a7be2bc417bb97c7631a
wcpun <puwach123@gmail.com>
2021-03-21 00:49:34 +0800

    add config default

--- src/command/getres.ts
+++ src/command/getres.ts
@@ -5,48 +5,49 @@ import Statisticer from '../statisticer/statisticer';
 import NormalizedMatches from '../type/normalize/matches';
 import NormalizedResults from '../type/normalize/results';
 
-function getResTemplate(
-  statisticer: (nmatches: NormalizedMatches) => NormalizedResults,
-  filter?: (nmatches: NormalizedMatches) => NormalizedMatches
-) {
-  return function(ipath: string, opath: string) {
-    try {
-      const importer = new JSONImporter<NormalizedMatches>(ipath);
-      const exporter = new JSONExporter<NormalizedResults>(opath);
-      let idata: NormalizedMatches = importer.import();
-      if (filter) idata = filter(idata);
-      const odata: NormalizedResults = statisticer(idata);
-      exporter.export(odata);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-}
+type FilterFunc = (nmatches: NormalizedMatches) => NormalizedMatches;
+type StatisticFunc = (nmatches: NormalizedMatches) => NormalizedResults;
 
-export const getHandicapRes = getResTemplate(Statisticer.getHandicapRes);
-export const getAwayHandicapRes = getResTemplate(
+const getResTmpl = (statisticer: StatisticFunc, ...filters: FilterFunc[]) => (
+  ipath: string,
+  opath: string
+) => {
+  try {
+    const importer = new JSONImporter<NormalizedMatches>(ipath);
+    const exporter = new JSONExporter<NormalizedResults>(opath);
+    let idata: NormalizedMatches = importer.import();
+    if (filters.length > 0) for (const filter of filters) idata = filter(idata);
+    const odata: NormalizedResults = statisticer(idata);
+    exporter.export(odata);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getHandicapRes = getResTmpl(Statisticer.getHandicapRes);
+export const getAwayHandicapRes = getResTmpl(
   Statisticer.getHandicapRes,
   Filter.onlyAway
 );
-export const getHostHandicapRes = getResTemplate(
+export const getHostHandicapRes = getResTmpl(
   Statisticer.getHandicapRes,
   Filter.onlyHost
 );
-export const getLastSixHandicapRes = getResTemplate(
+export const getLastSixHandicapRes = getResTmpl(
   Statisticer.getHandicapRes,
   Filter.lastSix
 );
 
-export const getHiLoRes = getResTemplate(Statisticer.getHiLoRes);
-export const getAwayHiLoRes = getResTemplate(
+export const getHiLoRes = getResTmpl(Statisticer.getHiLoRes);
+export const getAwayHiLoRes = getResTmpl(
   Statisticer.getHiLoRes,
   Filter.onlyAway
 );
-export const getHostHiLoRes = getResTemplate(
+export const getHostHiLoRes = getResTmpl(
   Statisticer.getHiLoRes,
   Filter.onlyHost
 );
-export const getLastSixHiLoRes = getResTemplate(
+export const getLastSixHiLoRes = getResTmpl(
   Statisticer.getHiLoRes,
   Filter.lastSix
 );
---
+++ src/config/default.ts
@@ -0,0 +1,12 @@
+const config: any = {};
+
+config.stat.ipath =
+  'C:\\Users\\admin\\Desktop\\scraper\\src\\data\\matchs.json';
+config.handicap.all.opath = `C:\\Users\\admin\\Desktop\\scraper\\src\\data\\handicap\\all.json`;
+config.handicap.host.opath = `C:\\Users\\admin\\Desktop\\scraper\\src\\data\\handicap\\host.json`;
+config.handicap.away.opath = `C:\\Users\\admin\\Desktop\\scraper\\src\\data\\handicap\\away.json`;
+config.hilo.all.opath = `C:\\Users\\admin\\Desktop\\scraper\\src\\data\\hilo\\all.json`;
+config.hilo.host.opath = `C:\\Users\\admin\\Desktop\\scraper\\src\\data\\hilo\\host.json`;
+config.hilo.away.opath = `C:\\Users\\admin\\Desktop\\scraper\\src\\data\\hilo\\away.json`;
+
+export default config;
